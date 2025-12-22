47e1d703bc679024a7ad4bd869c174586c793040
Sivaprakash Desingu <10977339+sivaprakashDesingu@users.noreply.github.com>
2025-12-22 22:52:15 +0530

    Chor(clean up): global header and footer
    Chor(clean up): global header and footer

--- config/settings.py
+++ config/settings.py
@@ -112,6 +112,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/stable/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/stable/ref/settings/#default-auto-field
